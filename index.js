fb957cb60dd1400d39aa63f3393bbf8eb86052b2
Markmutisya <mark.mwangi@dtudent.moringaschool.com>
2022-06-12 16:13:29 +0300

    Filter Array

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(myArray, myString)
+{  const matchingData = arrayContent => arrayContent.toUpperCase() === myString.toUpperCase();
+    return myArray.filter(matchingData)
+}
+    function fuzzyMatch (myArray, myString)
+    {
+        const matchingData = arrayContent => arrayContent.toLowerCase().charAt(0) === myString.toLowerCase().charAt(0);
+        return myArray.filter(matchingData)
+    }
+    function matchName(myArray,myString){
+        const matchingData = arrayContent => arrayContent.name === myString;
+        return myArray.filter(matchingData);
+    }
